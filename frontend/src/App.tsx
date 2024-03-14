781f38cfc761c2a001420ba9446699c8e9ac216d
jeffmikhail <mikhailcruz78@gmail.com>
2024-03-14 09:14:06 +0800

    project layout and custom font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,32 @@
+import { Routes, Route, BrowserRouter as Router, Navigate } from "react-router-dom";
+import Layout from "./layouts/Layout";
+
 function App() {
 	return (
-		<>
-			<h1 className="text-blue-600">Hello Tailwind</h1>
-		</>
+		<Router>
+			<Routes>
+				<Route
+					path=""
+					element={
+						<Layout>
+							<p>Home Page</p>
+						</Layout>
+					}
+				/>
+				<Route
+					path="/search"
+					element={
+						<Layout>
+							<p>Search Page</p>
+						</Layout>
+					}
+				/>
+				<Route
+					path="*"
+					element={<Navigate to={"/"} />}
+				/>
+			</Routes>
+		</Router>
 	);
 }
 
