2b16779f364727d4021003d0b1766534e81f5a52
jeffmikhail <mikhailcruz78@gmail.com>
2024-03-21 11:37:12 +0800

    ready for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,8 @@ import colors from "colors";
 import mongoose from "mongoose";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
+import path from "path";
 
-import { connectDB } from "./config/db";
 import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 7000;
 
 // db connection
-connectDB();
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 app.use(cookieParser());
 app.use(express.json());
@@ -25,10 +25,12 @@ app.use(
 	})
 );
 
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
 // routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
-	console.log(colors.cyan.italic.underline(`Server running on http://localhost:${PORT}`));
+app.listen(7000, () => {
+	console.log(colors.cyan.italic.underline(`Server running on http://localhost:7000`));
 });
